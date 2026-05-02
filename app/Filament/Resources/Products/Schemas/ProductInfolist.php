7a7bd81812a1c43b17bddb004e45a18f04a333a3
jonitodev <tuabangkamanangertiteknologi@gmail.com>
2026-05-03 00:28:12 +0700

    js7.3 praktikum & js7.3 tugas

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
-use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Section; 
+use Filament\Schemas\Components\Tabs;  
+use Filament\Schemas\Components\Tabs\Tab;  
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
@@ -15,6 +17,73 @@ class ProductInfolist
         return $schema
             ->components([
                 //
+                Tabs::make('Product Tabs')
+                    ->tabs([
+                        Tab::make('Product Details')
+                            ->icon('heroicon-o-information-circle')
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->label('Product Name')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                
+                                TextEntry::make('id')
+                                    ->label('Product ID'),
+        
+                                TextEntry::make('sku')
+                                    ->label('Product SKU')
+                                    ->badge()
+                                    ->color('success'),
+        
+                                TextEntry::make('description')
+                                    ->label('Product Description'),
+                                
+                                TextEntry::make('created_at')
+                                    ->label('Product Creation Date')
+                                    ->date('d M Y')
+                                    ->color('info'),
+                            ]),
+                        Tab::make('Product Price and Stock')
+                            ->icon('heroicon-o-currency-dollar')
+                            ->schema([
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-s-currency-dollar')
+                                    ->badge(fn ($record) => $record ? $record->stock : null), 
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->badge()
+                                    ->color('danger'),
+                            ]),
+                        Tab::make('Image and Status')
+                            ->icon('heroicon-o-photo')
+                            ->schema([
+                                ImageEntry::make('image')
+                                    ->label('Product Image')
+                                    ->disk('public'),
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-s-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                IconEntry::make('is_active')
+                                    ->label('Is Active?')
+                                    ->boolean(),
+                                IconEntry::make('is_featured')
+                                    ->label('Is Featured?')
+                                    ->boolean(),
+                            ]),
+                        
+                    ])
+                    ->columnSpanFull()
+                    ->vertical(),
+
                 Section::make('Product Info')
                     ->description('')
                     ->schema([
